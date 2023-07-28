57b99141899797296c8ef8360cabdf349b86da5c
Yakov Savelev (Laptop) <g6h6m238929@gmail.com>
2023-07-28 09:12:29 +0500

    Version 0.9: TEST

---
+++ plugins/c/random_c.pyi
@@ -0,0 +1,11 @@
+def randint(range_min:int, range_max:int) -> int:
+	"Return random integer"
+	...
+
+def choice(lst:list):
+	"Return random element from list"
+	...
+
+def _overclocking() -> None:
+	"Overclocking"
+	...
---
+++ plugins/c/rec.pyi
@@ -0,0 +1,3 @@
+def sorting_by_frequency(lst:list[int]) -> list[int]:
+	"Implementation of frequency sorting in C++"
+	...
