22091353f17c65120b8c86d30861ad122ce3738b
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-08-06 09:55:21 +0500

    Version 1.12.6

--- plugins/reader.py
+++ plugins/reader.py
@@ -1,7 +1,9 @@
 from plugins.wrapper import list_mass_search, sorting_by_frequency, get_indexes_table, get_all_indexes, damerau_levenshtein_distance
 
 def _database():
-    yield ...
+    with open('data.pylist', 'r', encoding='utf-8') as file:
+        for line in eval(file.read()):
+            yield line
 
 def search(request:str) -> list:
     result = []
