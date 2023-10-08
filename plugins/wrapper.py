b6731518dc1ef5ade8138a408db801d376d26f9e
Jacob Savel'ev <g6h6m238929@gmail.com>
2023-10-08 11:45:30 +0500

    Last commit (Version 0.16.1)

--- plugins/wrapper.py
+++
@@ -1,7 +0,0 @@
-from c.dl import damerau_levenshtein_distance
-from c.rec import sorting_by_frequency
-from c.random_c import randint, choice
-from c.algs import read, write, exists_file,\
-    longest_common_subsequnce, list_mass_search,\
-    dumps, dump, load, loads,\
-    get_all_indexes, get_indexes_table
